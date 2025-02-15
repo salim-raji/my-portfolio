06990ff66ff2630ec1fc93b207d89efc4f518b41
Salim Emmanuel Raji <salimraji@icloud.com>
2025-02-15 11:06:54 +0200

    --update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header/Header'
 import Home from './screens/HomeScreen'
+import './App.css'
 function App() {
 
   return (
-    <>
+    <div className="app">
       <Header/>
       <Home/>
-    </>
+    </div>
+
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,18 @@ import './Header.css'
 
 function Header(){
     return(
-        <div>
-            <h1>Hello World</h1>
+        <div className='header-container'>
+            <div className="header">
+                <div className="header-left">
+                    <button>IMAGE</button>
+                </div>
+                <div className="header-right">
+                    <a href="" className="nav-link">About Me</a>
+                    <a href="" className="nav-link">Work</a>
+                    <a href="" className="nav-link">Contact Me</a>
+                    <a href="" className="nav-link">CV</a>
+                </div>
+            </div>
         </div>
     )
 
